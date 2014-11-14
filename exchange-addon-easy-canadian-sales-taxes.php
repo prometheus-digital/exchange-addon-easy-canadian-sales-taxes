837c2aeea1227b63dddb474ecebbc50a9cd38428
root <root@petra.ithemes.com>
2014-11-14 10:12:14 -0600

    1.0.5: updated: lib/updater

--- exchange-addon-easy-canadian-sales-taxes.php
+++ exchange-addon-easy-canadian-sales-taxes.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: iThemes Exchange - Easy Canadian Sales Taxes
- * Version: 1.0.3
+ * Version: 1.0.5
  * Description: Adds Easy Canadian Sales Taxes to iThemes Exchange.
  * Plugin URI: http://ithemes.com/exchange/easy-canadian-sales-taxes/
  * Author: iThemes
