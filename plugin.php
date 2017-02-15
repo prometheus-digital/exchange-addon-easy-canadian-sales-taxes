78c672359b565b09577b26a64e9b07bc9e6581b2
Timothy Jacobs <timothy@ironbounddesigns.com>
2017-02-15 10:58:54 -0500

    Fix loading textdomain

--- plugin.php
+++ plugin.php
@@ -42,4 +42,5 @@ add_action( 'it_exchange_register_addons', 'it_exchange_register_easy_canadian_s
 function it_exchange_easy_canadian_sales_taxes_set_textdomain() {
 	load_plugin_textdomain( 'LION', false, dirname( plugin_basename( __FILE__  ) ) . '/lang/' );
 }
-add_action( 'plugins_loaded', 'it_exchange_easy_canadian_sales_taxes_set_textdomain' );
+
+it_exchange_easy_canadian_sales_taxes_set_textdomain();
